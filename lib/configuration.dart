84c05cc7ba6981cda9c0f974b80fd1f09a33cb07
Ramineni Sunanda <sramineni@gainsight.com>
2023-07-07 13:48:12 +0530

    - Bumped iOS SK to 1.9.4 - Reading sdk version from runScript in iOS SDK

--- lib/configuration.dart
+++ lib/configuration.dart
@@ -1,22 +1,21 @@
 class Configurations {
   Configurations(this.apiKey,
       {this.flushInterval = 30,
-      this.flushQueueSize = 20,
-      this.maxQueueSize = 1000,
-      this.enableLogs = false,
-      this.trackApplicationLifeCycleEvents = true,
-      this.shouldTrackTapEvents = false,
-      this.reportTrackingIssues = false,
-      this.enable = true,
-      this.host = PXHost.us,
-      this.collectDeviceId = true});
+        this.flushQueueSize = 20,
+        this.maxQueueSize = 1000,
+        this.enableLogs = false,
+        this.trackApplicationLifeCycleEvents = true,
+        this.shouldTrackTapEvents = false,
+        this.reportTrackingIssues = false,
+        this.enable = true,
+        this.collectDeviceId = true});
 
   Configurations.fromJson(Map<String, dynamic> json)
       : apiKey = json['apiKey'],
         flushInterval = json['flushInterval'],
         flushQueueSize = json['flushQueueSize'],
         maxQueueSize = json['maxQueueSize'],
-        enableLogs = json['enableLogs'],
+      enableLogs = json['enableLogs'],
         trackApplicationLifeCycleEvents =
             json['trackApplicationLifeCycleEvents'],
         shouldTrackTapEvents = json['shouldTrackTapEvents'],
@@ -26,18 +25,18 @@ class Configurations {
         proxy = json['proxy'],
         host = json['host'];
 
-  final String apiKey;
-  int flushInterval;
-  int flushQueueSize;
-  int maxQueueSize;
-  bool enableLogs;
-  bool trackApplicationLifeCycleEvents;
-  bool shouldTrackTapEvents;
-  bool reportTrackingIssues;
-  bool enable;
-  bool collectDeviceId;
+  final String? apiKey;
+  int flushInterval = 30;
+  int flushQueueSize = 20;
+  int maxQueueSize = 1000;
+  bool enableLogs = false;
+  bool trackApplicationLifeCycleEvents = true;
+  bool shouldTrackTapEvents = false;
+  bool reportTrackingIssues = false;
+  bool enable = true;
+  bool collectDeviceId = true;
   String? proxy;
-  PXHost host;
+  PXHost? host;
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> jsonObj = {
@@ -52,7 +51,8 @@ class Configurations {
       'enable': enable,
       'collectDeviceId': collectDeviceId,
       'proxy': proxy,
-      'host': host.name
+      // ignore: prefer_null_aware_operators
+      'host': host?.name
     }..removeWhere((key, value) => value == null);
     return jsonObj;
   }
